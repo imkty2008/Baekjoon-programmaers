a1424320c632244f90a70bf5486a22ec0c36602d
imkty2008 <165138775+imkty2008@users.noreply.github.com>
2024-06-26 16:23:59 +0900

    Create Str.java

---
+++ Str.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Str {
+         public static void main(String args[]) {
+                  Scanner sc = new Scanner(System.in);
+                  String x, y, z = " ";
+                  System.out.print("문자를 입력하세요 :")'
+                  x = sc.nextInt();
+                  System.outr.print("문자를 입력하세요 :");
+                  y = sc.nextInt();
+
+                  z = x;
+                  x = y;
+                  y = z;
+ 
+                  System.out.print(x +" "+ y);
+         }
+}
+                  
