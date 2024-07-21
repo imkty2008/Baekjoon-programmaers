fd35b4438a0c7ce49a3f48f29bac41099bb697d7
imkty2008 <165138775+imkty2008@users.noreply.github.com>
2024-07-21 14:04:00 +0900

    Update Dollar_1101.java

--- Dollar_1101.java
+++ Dollar_1101.java
@@ -5,7 +5,7 @@ public class Dollar_1101 {
         Scanner sc = new Scanner(System.in);
 	     	System.out.println("원화를 입력하세요(단위:원) :");
 		    double x = sc.nextInt();
-	    	double y = x / 1260;
+	    	    double y = x / 1260;
 		    double z = (int)(y * 100 + 0.5) / 100.0;
 		    System.out.println(m+"원은 $"+z+"입니다.");
 	  }
