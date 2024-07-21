31233204d2e8f218f3d4feb9380db41f6095fdc4
imkty2008 <165138775+imkty2008@users.noreply.github.com>
2024-07-21 14:07:57 +0900

    Update Dollar_1101.java

--- Dollar_1101.java
+++ Dollar_1101.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
 public class Dollar_1101 {
-   public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-	     	System.out.println("원화를 입력하세요(단위:원) :");
-		    double x = sc.nextInt();
-	    	    double y = x / 1260;
-		    double z = (int)(y * 100 + 0.5) / 100.0;
-		    System.out.println(m+"원은 $"+z+"입니다.");
-	  }
+     public static void main(String args[]) {
+          Scanner sc = new Scanner(System.in);
+		System.out.println("원화를 입력하세요(단위:원) :");
+		double x = sc.nextInt();
+		double y = x / 1260;
+		double z = (int)(y * 100 + 0.5) / 100.0;
+		System.out.println(x + "원은 $" + z + "입니다.");
+      }
 }
 		
