70ff24965637f686eaadd9f68be5c3d7f23275a2
imkty2008 <165138775+imkty2008@users.noreply.github.com>
2024-07-29 22:40:25 +0900

    Create ForTest_1.java

---
+++ ForTest_1.java
@@ -0,0 +1,9 @@
+public class ForTest_1 {
+	public static void main(String args[]) {
+	              int n = 0;
+	              for(int i = 1; i <= 10; i++) {
+	              n += i;
+	              }
+	              System.out.println("1부터 10까지의 합은 : "+ n);
+	       }
+	}
