3116ba0bb94832af9a360db4c4a17d3b88234276
imkty2008 <165138775+imkty2008@users.noreply.github.com>
2024-07-29 22:51:56 +0900

    Create ForTest_2.java

---
+++ ForTest_2.java
@@ -0,0 +1,11 @@
+pulic class ForTest_2 {
+      public static void main(String args[]) {
+             int x = 1;
+             int y = 10;
+             for(int i = 0; i < 10; i++) {
+             System.out.println(x + "\t" + y);
+             x++;
+             y--;
+             }
+       }
+}
